bc9cd4d125a4611684d9df6ebbb8e1bfee528ec9
charly <cbarrionuevo@fidelatam.com>
2023-12-06 09:05:26 -0300

    Se crea componente ppal & componente OwnersApp p/ gestion de organizadores

---
+++ src/RaceCalendarApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import OwnersApp from './components/OwnersApp'
+
+const RaceCalendarApp = () => {
+  return (
+    <div>
+      <OwnersApp/>
+    </div>
+  )
+}
+
+export default RaceCalendarApp
---
+++ src/components/OwnersApp.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const OwnersApp = () => {
+  return (
+    <div>
+      <h1>App de owners</h1>
+    </div>
+  )
+}
+export default OwnersApp
